f051c2295a787b6555bedf682ce6b610abfdbd09
Genicleito <genicleito@live.com>
2014-10-10 16:12:00 -0300

    Update Aula_Dia_08.asm

--- Aula_Dia_08.asm
+++ Aula_Dia_08.asm
@@ -1,61 +1,77 @@
 ;Aula-08/10/14
-;Autor: Genicleito Gonçalves
+;Genicleito Gonçalves
 
 %include "asm_io.inc"
 
 segment .data
 
+Texto1	db	"Digite 5 numeros:", 0
+
 segment .bss
-	vetor	resd		5
+	vetor	resd	5
 	
 segment .text
     global  asm_main
 	
 asm_main:
 
+	mov	eax,	Texto1
+	call	print_string
+	call	print_nl	
+
+	mov	ecx,	5
+
 	cld
-	mov	 edi, vetor
-	mov ecx, 5
-	LP:
-		call read_int
+	mov	edi,	vetor
+
+	L1:
+		call	read_int
 		stosd
-	loop LP
-	
-	mov ecx, 4
-	
-	LP3:
-	
-		mov edi, vetor
-		mov esi, vetor
-		add esi, 4
-		sub edi, 4	;para não comparar com o proximo
-		sub esi, 4	;para não comparar com o proximo
+	loop L1
+
+	mov	ecx,	5
+	LPComparacao:
+
+		mov	ebx,	ecx
 		
-		mov ecx, 4
-		
-		LP2:
+		mov	esi,	vetor	;esi guarda uma posição antes do edi
+		mov	edi,	vetor
+		add	edi,	4	;edi guarda uma posiçao a frente do esi	
+
+		mov	ecx,	4
+		LPRepeticao:
 			cmpsd
-			jle	n_troca
-			mov eax, [EDI]
-			mov ebx, [esi]
-			mov [edi], ebx
-			mov [esi], eax
-			
-			n_troca:
-				add esi, 4
-				add edi, 4
-		loop LP2
-		
-	loop LP3
-	
-	mov esi, vetor
-	mov ecx, 5
-	
-	LP4:
-		lodsd
-		call print_int
-		call print_nl
-	loop LP4
+			jg	Troca	;O valor anterior é maior do que o proximo
+			Continua:
+		loop LPRepeticao
+
+		mov	ecx,	ebx
+	loop	LPComparacao
+
+	jmp	MostrarESair
+
+	Troca:
+		sub	esi,	4
+		sub	edi,	4
+		mov	edx,	[edi]
+		mov	eax,	[esi]
+		mov	[esi],	edx
+		mov	[edi],	eax
+
+		add	esi,	4
+		add	edi,	4		
+
+		jmp	Continua
+
+	MostrarESair:
+		mov	ecx,	5
+		mov	esi,	vetor
+		call	print_nl
+	Repetir:
+		lodsd		
+		call	print_int
+		call	print_nl
+	loop	Repetir
 
 	leave
 	ret

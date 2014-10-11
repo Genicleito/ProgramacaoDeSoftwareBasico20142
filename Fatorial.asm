91c15675bf9d502af96348132c71ae099f171635
Genicleito <genicleito@live.com>
2014-10-10 23:23:25 -0300

    Create Fatorial.asm

---
+++ Fatorial.asm
@@ -0,0 +1,32 @@
+;Genicleito Gonçalves
+	
+%include "asm_io.inc"
+
+segment .data
+
+	Texto db "Digite um número: ", 0
+
+segment .bss
+	
+segment .text
+	global asm_main
+	
+asm_main:
+	
+	mov	eax,	Texto
+	call	print_string
+
+	call	read_int
+	
+	mov	ecx,	eax
+	dec	ecx
+
+	While:
+		mul	ecx
+	loop	While
+
+	call	print_int
+	call	print_nl
+	
+	leave
+	ret

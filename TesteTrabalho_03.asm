4810510d2538dfaa77fc97903ae575d947ad9e43
Genicleito <genicleito@live.com>
2014-10-22 19:16:26 -0200

    Update TesteTrabalho_03.asm

--- TesteTrabalho_03.asm
+++ TesteTrabalho_03.asm
@@ -14,7 +14,19 @@ segment .text
 	
 asm_main:
 
+	;#Se for colchetes ou parenteses coloca no topo da pilha	;Opcional
+	;Se for operando coloca no vetor de pós fixa
+	;Se for operador coloca no topo da pilha
+	;#OBSERVAÇÂO: se já tiver um operador no topo da pilha e ele tiver prioridade, ele será colocado no vetor de pós fixa e o lido deverá ser colocado no topo
+	;Se for lido um parenteses fechando então desempilha o operador do topo da pilha e coloca no vetor de pós fixa
 
+	mov	eax,	msg1
+	call	print_string
+	
+	mov	ecx, 20
+	lp:
+		call read_char
+	loop lp
 
 	call	print_nl
 	leave

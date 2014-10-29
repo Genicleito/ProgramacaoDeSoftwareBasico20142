6ee2d8c5673e4ad8a113c5a43dc77e0fb79de12e
Genicleito <genicleito@live.com>
2014-10-29 19:13:53 -0200

    Update Fibonacci.asm

--- Fibonacci.asm
+++ Fibonacci.asm
@@ -9,7 +9,26 @@ segment .text
         global  asm_main
 asm_main:
 
-
+        call    read_int
+        push    eax
+        call    fibonacci
+        jmp     Sair
+        
+        fibonacci:
+                push    ebp
+                mov     ebp,    esp
+                mov     ebx,    [ebp + 8]
+                cmp     ebx,    1
+                jle     Fim
+                
+                Fim:
+                        pop     ebp
+                        ret
+                
+        
+Sair:
+        pop     eax
+        call    print_int
         
 leave                     
 ret
